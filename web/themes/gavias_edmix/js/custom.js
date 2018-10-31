5c208b2602a1df08ce5756b4ef4cfac4fe64dab2
William Bautista <wbautista@seedem.co>
2018-10-30 21:05:05 -0500

    Ajuste

--- web/themes/gavias_edmix/js/custom.js
+++ web/themes/gavias_edmix/js/custom.js
@@ -7,29 +7,45 @@
         select_2 = '.field--name-dynamic-block-fieldnode-redes-sociales',
         select_3 = '.block-views-blockgrupo-contenido-block-4',
         select_4 = '.block-system-main-block .node--type-news',
+        select_5 = '.node--type-event',
+        select_6 = '.field--type-datetime time',
         
         query_1 = window.matchMedia("(max-width: 902px)");
 
     function queryMatch(query) {
         if (query.matches) {
-            document.querySelector(select_3).appendChild(document.querySelector(select_2));
+            $(select_3).append($(select_2));
         } else {
-            document.querySelector(select_4).appendChild(document.querySelector(select_2));
+            $(select_4).append($(select_2));
         }
     }
 
     UniCustom = (function() {
         return {
+            // Página de noticias
             news_page: function() {
                 queryMatch(query_1);
                 query_1.addListener(queryMatch);
+            },
+            // Página de eventos
+            event_page: function() {
+                $(select_6).map( (ind, obj) => {
+                    var monthNumber = $(obj).text().split('-'),
+                        monthName = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
+                        newDate = monthNumber[1] + '<span>' + monthName[parseInt(monthNumber[0] - 1)] + '</span>';
+                    $(obj).html(newDate);
+                });
+                
             }
         };
     }());
 
     window.onload = function() {
-        if (document.querySelector(select_1)) {
+        if ($(select_1)) {
             UniCustom.news_page();
+        }
+        if ($(select_5)) {
+            UniCustom.event_page();
         }
     };
 }(jQuery, window));
